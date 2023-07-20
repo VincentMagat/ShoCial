4862c1bbaba30b7fd8416d25f0deab05ba5ae784
vinxxxz <junevincent.magat@gmail.com>
2023-07-20 00:58:11 -0700

    designed landing page still ongoin

---
+++ src/main/java/com/shocial/shocial/ShocialApplication.java
@@ -0,0 +1,13 @@
+package com.shocial.shocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shocial/shocial/ShocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shocial.shocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
